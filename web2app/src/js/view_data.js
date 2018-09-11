0ffc4e73fbeb87427975e1ecc00de79a685fb500
qupengyu <942163814@qq.com>
2018-09-11 18:33:28 +0800

    详情

---
+++ web2app/src/js/view_data.js
@@ -0,0 +1,35 @@
+var view_data = {
+	menu_msglist:{
+		getapi : 'notifyInfo',
+		title : '提示信息',
+		field : [
+			{
+				title:'信息标题',
+				type:'text',
+				code:'title'
+			},
+			{
+				title:'模块名称',
+				type:'text',
+				code:'module'
+			},
+			{
+				title:'查看时间',
+				type:'text',
+				code:'cksj'
+			},
+			{
+				title:'添加时间',
+				type:'text',
+				code:'addtime'
+			},
+			{
+				title:'信息内容',
+				type:'cont',
+				code:'neir'
+			}
+		]
+	}
+}
+
+export default view_data;
--- web2app/src/template/view.js
+++ web2app/src/template/view.js
@@ -2,22 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import app from '../js/app.js';
 import Header from '../comp/header.js';
+import ViewData from '../js/view_data.js';
 
 class View extends React.Component{
 
 	constructor(props){
 		super(props);
+
+		const module = app.getQueryString("module");
+		
+		const {title} = ViewData[module];//标题
+		const {field} = ViewData[module];//字段
+		const {getapi} = ViewData[module];
+
 		this.state = {
-			title: '详情'
+			title:title,
+			field:field,
+			getapi:getapi,
+			vdata:{}
 		}
 	}
 
-	render(){
+	componentDidMount(){
+		app.ajax({
+			url : app.domain + '/' + this.state.getapi + '?bm=' + app.getQueryString("code"),
+			success:(data)=>{
+				if(data.outOk == '1') {
+					if(data.outOk == '1'){
+						this.setState({
+							vdata:data
+						})
+					}
+				}
+			}
+		});
+	}
 
+	render(){
 		return(
-			<Header titleName={this.state.title} rbut="show"/>
+			<div>
+				<Header titleName={this.state.title} rbut="show"/>
+				<div className="mui-input-group">
+				{
+					this.state.field.map((item) => {
+						return (
+							<Field key={item.code} item={item} />
+						)
+					})
+				}
+				</div>
+			</div>
 		)
 
+	}
+
+}
+
+class Field extends React.Component{
+
+	constructor(props){
+		super(props)
+	}
+
+	render(){
+		const {type} = this.props.item;
+		const {title} = this.props.item;
+		let itemDom = (<div></div>);
+		if(type === 'text'){//文本
+			itemDom = (<div className="mui-input-row">
+				<label>{title}</label>
+				<div className="wrapdiv info"></div>
+			</div>)
+		}
+
+		return(
+			itemDom
+		)
 	}
 
 }
