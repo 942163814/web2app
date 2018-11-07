13cc77bb96c063c0238c4924f88f2fdebdbed7ad
942163814 <942163814@qq.com>
2018-11-08 00:36:36 +0800

    列表demo

--- web2app/src/index.js
+++ web2app/src/index.js
@@ -8,11 +8,8 @@ import {
 import { Redirect } from 'react-router';
 import Home from './frame/index-home.js'; //首页
 import List from './template/list.js'; //列表
-<<<<<<< HEAD
 import ListDemo from './template/listdemo.js';
-=======
 import View from './template/view.js'; //详情
->>>>>>> 0180cab7c35c0d25465ccd66ec7c45d948745533
 require('./css/mui.css');
 require('./css/app.css');
 require('./css/icons-extra.css');
@@ -23,12 +20,8 @@ ReactDOM.render(
 		<Router>
 			<div>
 				<Route exact path='/risdz/view/build/index.html' component={Home} />
-<<<<<<< HEAD
 				<Route exact path='/list' component={ListDemo} />
-=======
-				<Route exact path='/list' component={List} />
 				<Route exact path='/view' component={View} />
->>>>>>> 0180cab7c35c0d25465ccd66ec7c45d948745533
 			</div>
 		</Router>
 	</div>
